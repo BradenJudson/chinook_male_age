2f4525fcb469496a7319e498da8058884e9654c4
Judson <Braden.Judson@dfo-mpo.gc.ca>
2024-04-23 12:07:15 -0700

    reorganize shell scripts for genotyping

--- genotyping/07_alignment.sh
+++ genotyping/07_alignment.sh
@@ -6,7 +6,7 @@
 #SBATCH --mem-per-cpu=5G
 #SBATCH --ntasks=1
 #SBATCH --mail-user=Braden.Judson@dfo-mpo.gc.ca
-#SBATCH --time=96:00:00
+#SBATCH --time=120:00:00
 #SBATCH --cpus-per-task=64
 #SBATCH --account=grdi_genarcc
 
